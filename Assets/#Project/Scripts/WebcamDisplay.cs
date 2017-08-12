5370f2fbfa0d5c41f4af6c7bfe79a00e227a1010
sayangel <sayangel@gmail.com>
2017-08-11 21:14:24 -0500

    webcam sample scene

---
+++ Assets/#Project/Scripts/WebcamDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamDisplay : MonoBehaviour {
+
+	[SerializeField] private Renderer _renderer;
+
+	void Start () {
+		
+		WebCamTexture _webcamTexture = new WebCamTexture();
+		_renderer.material.mainTexture = _webcamTexture;
+		_webcamTexture.Play ();
+
+		/* We can use this to debug camera issues.
+		foreach (WebCamDevice device in WebCamTexture.devices) {
+			Debug.Log ("device name " + device.name);
+		}*/
+	}
+
+}
