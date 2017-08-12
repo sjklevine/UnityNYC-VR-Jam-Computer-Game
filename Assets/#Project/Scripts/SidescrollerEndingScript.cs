ea87ee8aa448c2d33241c444be0c060268d13948
LAPTOP-8GA8M8OM\User <nyerds@gmail.com>
2017-08-12 19:14:36 -0400

    Stop soundtrack at end of level.

--- Assets/#Project/Scripts/SidescrollerEndingScript.cs
+++ Assets/#Project/Scripts/SidescrollerEndingScript.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SidescrollerEndingScript : MonoBehaviour {
 	public SimpleTimedToggleVisibilty endgameText;
-
+    public AudioSource Soundtrack;
+ 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag.Equals("Player")) {
 			endgameText.BeginToggling ();
             endgameText.GetComponent<AudioSource>().Play();
+            Soundtrack.Stop();
 		}
 	}
 }
