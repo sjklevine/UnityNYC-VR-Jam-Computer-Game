c6cb781ef1e760fe00bf062a8ba6d40dfcd16a5e
sayangel <sayangel@gmail.com>
2017-08-13 13:07:08 -0500

    arrows always on

--- Assets/#Project/Scripts/PlatformGrabHelper.cs
+++ Assets/#Project/Scripts/PlatformGrabHelper.cs
@@ -6,13 +6,14 @@ public class PlatformGrabHelper : MonoBehaviour {
 
 	[SerializeField] GameObject helperArrows;
 	void Start (){
+		helperArrows.SetActive(true);
 		if (GetComponent<VRTK_InteractableObject>() == null){
 			Debug.LogError("Required to be attached to an Object that has the VRTK_InteractableObject script attached to it");
 			return;
 		}
 
-		GetComponent<VRTK_InteractableObject>().InteractableObjectTouched += new InteractableObjectEventHandler(ObjectGrabbed);
-		GetComponent<VRTK_InteractableObject>().InteractableObjectUntouched += new InteractableObjectEventHandler(ObjectUngrabbed);
+		//GetComponent<VRTK_InteractableObject>().InteractableObjectTouched += new InteractableObjectEventHandler(ObjectGrabbed);
+		//GetComponent<VRTK_InteractableObject>().InteractableObjectUntouched += new InteractableObjectEventHandler(ObjectUngrabbed);
 	}
 
 	private void ObjectGrabbed(object sender, InteractableObjectEventArgs e){
