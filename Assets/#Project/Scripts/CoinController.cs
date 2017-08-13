a928cf92b731f41eef4d97977e94d57a7ab2b902
sayangel <sayangel@gmail.com>
2017-08-13 08:31:36 -0500

    updated coin prefab with disappear behavior, UI update delegate, and sound

---
+++ Assets/#Project/Scripts/CoinController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	public static Action<int> OnCoinTotalUpdate;
+
+	private static int _totalCoinsCollected = 0;
+	[SerializeField] AudioClip _audio;
+
+	void OnTriggerEnter(Collider col){
+		Destroy (gameObject);
+		AudioSource.PlayClipAtPoint(_audio, transform.position);
+		_totalCoinsCollected++;
+		if (OnCoinTotalUpdate != null)
+			OnCoinTotalUpdate (_totalCoinsCollected);
+	}
+}
